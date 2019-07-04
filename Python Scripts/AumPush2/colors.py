2f6e9af3092f1bb413d458844307d5aee42d6ad6
aumhaa <aumhaa@gmail.com>
2019-07-04 13:22:49 -0700

    AumPush2  :   added button colors for TaggedDefaultsComponent component in colors.py.

--- Python Scripts/AumPush2/colors.py
+++ Python Scripts/AumPush2/colors.py
@@ -1,4 +1,4 @@
- 
+
 #from __future__ import absolute_import, print_function
 from functools import partial
 from ableton.v2.control_surface import Skin
@@ -27,6 +27,14 @@ UNLIT_COLOR = Rgb.BLACK
 class Colors(ColorsBase):
 
 
+	class TaggedDefaults:
+
+		ResetOff = Rgb.RED
+		ResetOn = Rgb.WHITE
+
+
+
+
 	class Mod:
 
 		ShiftOn = Basic.ON
@@ -37,8 +45,8 @@ class Colors(ColorsBase):
 		class Nav:
 			OnValue = Rgb.RED
 			OffValue = Rgb.WHITE
-		
-	
+
+
 
 	class MonoInstrument:
 
@@ -56,7 +64,7 @@ class Colors(ColorsBase):
 			RootBlackValue = Rgb.RED_SHADE
 			WhiteValue = Rgb.SKY
 			BlackValue = Rgb.BLUE
-	
+
 
 		class Drums:
 			SelectedNote = Rgb.BLUE
@@ -69,6 +77,3 @@ def make_default_skin():
 
 def make_drum_pad_coloring_skin():
 	return Skin(ColorsWithDrumPadColoring)
-
-
-
