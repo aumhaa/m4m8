99fbfeb3256e8a49ab4a38d80e8d4798c13e7134
aumhaa <aumhaa@gmail.com>
2021-11-08 22:44:21 -0800

    Novation mod scripts   :   removal from repo (deprecated by Aum256 override capabilities),.

--- Python Scripts Live11/LaunchMod_Pro/LedLightingComponent.py
+++
@@ -1,9 +0,0 @@
-
-
-from ableton.v2.control_surface.control import ButtonControl
-#from _Framework.Control import ButtonControl
-from ableton.v2.control_surface import Component
-#from _Framework.ControlSurfaceComponent import ControlSurfaceComponent
-
-class LedLightingComponent(Component):
-    button = ButtonControl(color='Misc.Shift', pressed_color='Misc.ShiftOn')
